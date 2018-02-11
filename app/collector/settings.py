f69ec8961b39f671b2d147fcd585dc25d9b3a682
Viktor Petersson <vpetersson@wireload.net>
2018-02-11 17:42:47 +0000

    Static tweaks.

--- app/collector/settings.py
+++ app/collector/settings.py
@@ -163,6 +163,7 @@ CELERY_BEAT_SCHEDULE = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
