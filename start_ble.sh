1fdd4c7cec52869e612694f1a2fe5c46db89b68f
Viktor Petersson <vpetersson@wireload.net>
2018-02-18 17:06:25 +0000

    More ble/celery refactoring.

--- start_ble.sh
+++ start_ble.sh
@@ -6,3 +6,28 @@ rmmod hci_uart btbcm bluetooth xt_tcpudp veth
 # Start up
 hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -
 hciconfig hci0 up
+
+SOFT_COUNTER=0
+SOFT_COUNTER_LIMIT=3
+
+while [ "$SOFT_COUNTER" -lt "$SOFT_COUNTER_LIMIT" ]; do
+    echo "Scan for devices..."
+    if [ $(hcitool scan | wc -l) -le 1 ]; then
+        FAILED=1
+    else
+        FAILED=0
+    fi
+
+    # Test result
+    if [ $FAILED -eq 1 ]; then
+        echo "Initialization failed."
+
+        let SOFT_COUNTER=SOFT_COUNTER+1
+        echo "Soft error counter: $SOFT_COUNTER/$SOFT_COUNTER_LIMIT"
+
+        sleep $(shuf -i1-60 -n1)
+    else
+        echo "Initialization successful."
+        break
+    fi
+done
