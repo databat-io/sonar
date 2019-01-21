c700f6c3f0f8f1202ce62a79495735afee070c3e
Viktor Petersson <vpetersson@wireload.net>
2019-01-21 20:53:49 +0000

    Bumps up sqlite timout

--- app/collector/settings.py
+++ app/collector/settings.py
@@ -113,6 +113,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(DATABASE_PATH, 'db.sqlite3'),
+        'OPTIONS': {
+            'timeout': 20,  # in seconds
+        }
     }
 }
 
