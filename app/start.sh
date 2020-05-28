62c55a1131f0a489163e95d3819d5d82f5648850
Viktor Petersson <self@vpetersson.com>
2020-05-28 14:32:12 +0100

    Adds new container and more permission to celery

--- app/start.sh
+++ app/start.sh
@@ -5,78 +5,90 @@ mkdir -p \
     /data/redis \
     /data/collector
 
-# Run migrations first
-python manage.py migrate
-python manage.py collectstatic --noinput
+if [ "$GUNICORN" = "1" ]; then
 
+    # Run migrations first
+    python manage.py migrate
+    python manage.py collectstatic --noinput
 
-SOFT_COUNTER=0
-SOFT_COUNTER_LIMIT=3
+    /usr/local/bin/gunicorn \
+        --access-logfile - \
+        --bind 0.0.0.0:80 \
+        --pid /tmp/pid-gunicorn \
+        collector.wsgi:application
 
-while [ "$SOFT_COUNTER" -lt "$SOFT_COUNTER_LIMIT" ]; do
-    echo "Attaching hci0..."
-    /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -
+elif [ "$CELERY" = "1" ]; then
 
-    echo "Bring hci0 up..."
-    hciconfig hci0 up
+    SOFT_COUNTER=0
+    SOFT_COUNTER_LIMIT=3
 
-    echo "Scan for devices..."
-    if [ $(hcitool scan | wc -l) -le 1 ]; then
-        FAILED=1
-    else
-        FAILED=0
-    fi
+    while [ "$SOFT_COUNTER" -lt "$SOFT_COUNTER_LIMIT" ]; do
+        echo "Attaching hci0..."
+        /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -
 
-    # Test result
+        echo "Bring hci0 up..."
+        hciconfig hci0 up
+
+        echo "Scan for devices..."
+        if [ $(hcitool scan | wc -l) -le 1 ]; then
+            FAILED=1
+        else
+            FAILED=0
+        fi
+
+        # Test result
+        if [ $FAILED -eq 1 ]; then
+            echo "Initialization failed."
+
+            let SOFT_COUNTER=SOFT_COUNTER+1
+            echo "Soft error counter: $SOFT_COUNTER/$SOFT_COUNTER_LIMIT"
+
+            sleep $(shuf -i1-60 -n1)
+        else
+            echo "Initialization successful."
+            break
+        fi
+    done
+
+    # Primitive error handling
     if [ $FAILED -eq 1 ]; then
-        echo "Initialization failed."
 
-        let SOFT_COUNTER=SOFT_COUNTER+1
-        echo "Soft error counter: $SOFT_COUNTER/$SOFT_COUNTER_LIMIT"
+        HARD_COUNTER_FILE=/tmp/restart_counter
+        SLEEP=$(shuf -i1-300 -n1)
 
-        sleep $(shuf -i1-60 -n1)
-    else
-        echo "Initialization successful."
-        break
+        if [ ! -f "$HARD_COUNTER_FILE" ]; then
+            echo 1 > "$HARD_COUNTER_FILE"
+            HARD_COUNTER=1
+        else
+            HARD_COUNTER="$(cat $HARD_COUNTER_FILE)"
+            let HARD_COUNTER=$HARD_COUNTER+1
+            echo $HARD_COUNTER > $HARD_COUNTER_FILE
+        fi
+
+        echo "Hard error counter: $HARD_COUNTER/5"
+        echo "All tests failed. Taking hard action in $SLEEP seconds."
+        sleep $SLEEP
+
+        if [  $HARD_COUNTER -lt 5 ]; then
+            # Resin restart command
+            echo "Restarting container."
+            curl -X POST --header "Content-Type:application/json" \
+                "$RESIN_SUPERVISOR_ADDRESS/v1/restart?apikey=$RESIN_SUPERVISOR_API_KEY"
+        else
+            echo "Restarting device."
+            # Resin reboot command
+            curl -X POST --header "Content-Type:application/json" \
+                "$RESIN_SUPERVISOR_ADDRESS/v1/reboot?apikey=$RESIN_SUPERVISOR_API_KEY"
+        fi
     fi
-done
 
-# Primitive error handling
-if [ $FAILED -eq 1 ]; then
 
-    HARD_COUNTER_FILE=/tmp/restart_counter
-    SLEEP=$(shuf -i1-300 -n1)
-
-    if [ ! -f "$HARD_COUNTER_FILE" ]; then
-        echo 1 > "$HARD_COUNTER_FILE"
-        HARD_COUNTER=1
-    else
-        HARD_COUNTER="$(cat $HARD_COUNTER_FILE)"
-        let HARD_COUNTER=$HARD_COUNTER+1
-        echo $HARD_COUNTER > $HARD_COUNTER_FILE
-    fi
-
-    echo "Hard error counter: $HARD_COUNTER/5"
-    echo "All tests failed. Taking hard action in $SLEEP seconds."
-    sleep $SLEEP
-
-    if [  $HARD_COUNTER -lt 5 ]; then
-        # Resin restart command
-        echo "Restarting container."
-        curl -X POST --header "Content-Type:application/json" \
-            "$RESIN_SUPERVISOR_ADDRESS/v1/restart?apikey=$RESIN_SUPERVISOR_API_KEY"
-    else
-        echo "Restarting device."
-        # Resin reboot command
-        curl -X POST --header "Content-Type:application/json" \
-            "$RESIN_SUPERVISOR_ADDRESS/v1/reboot?apikey=$RESIN_SUPERVISOR_API_KEY"
-    fi
+    /usr/local/bin/celery \
+        -A collector \
+        worker \
+        -l info \
+        --concurrency=2 \
+        --beat
+else
+    echo "Unknown runtime."
 fi
-
-
-/usr/local/bin/celery \
-    -A collector \
-    worker \
-    -l info \
-    --concurrency=2 \
-    --beat
