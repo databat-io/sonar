2801f5b96dd760868b6836eeb051e85d6beae908
Viktor Petersson <self@vpetersson.com>
2020-05-29 10:56:22 +0100

    Reverts udev and balena-sound settings

--- app/start.sh
+++ app/start.sh
@@ -21,20 +21,6 @@ elif [ "$CELERY" = "1" ]; then
     export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
     export UDEV=1
 
-    # Set the discoverable timeout here
-    dbus-send --system --dest=org.bluez --print-reply /org/bluez/hci0 org.freedesktop.DBus.Properties.Set string:'org.bluez.Adapter1' string:'DiscoverableTimeout' variant:uint32:0 > /dev/null
-
-    printf "Restarting bluetooth service\n"
-    service bluetooth restart > /dev/null
-    sleep 2
-
-    # Redirect stdout to null, because it prints the old BT device name, which
-    # can be confusing and it also hides those commands from the logs as well.
-    printf "discoverable on\npairable on\nexit\n" | bluetoothctl > /dev/null
-
-    # Start bluetooth and audio agent
-    /usr/src/bluetooth-agent &
-
     # Temporary for debugging.
     sleep 3600
 
