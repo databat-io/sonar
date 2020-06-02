d1c494ed4305043d3eb3e0e074b643350af6a2b4
Viktor Petersson <self@vpetersson.com>
2020-06-02 16:17:58 +0100

    Replace redis lock for simple DYI

--- app/django/ble/lib/ble_helper.py
+++ app/django/ble/lib/ble_helper.py
@@ -2,11 +2,24 @@ from bluepy.btle import Scanner, DefaultDelegate, BTLEManagementError
 from django.conf import settings
 import redis
 
+LOCK_NAME = 'ble-lock'
 r = redis.Redis(
     host=settings.REDIS_HOST,
     port=settings.REDIS_PORT,
     db=settings.REDIS_DATABASE
 )
+
+
+def set_lock(timeout):
+    if r.get(LOCK_NAME):
+        return False
+    else:
+        r.set(LOCK_NAME, 1)
+        r.expire(timeout)
+        return True
+
+def delete_lock():
+    r.delete(LOCK_NAME)
 
 
 def scan_for_btle_devices(timeout=30):
@@ -15,15 +28,16 @@ def scan_for_btle_devices(timeout=30):
             DefaultDelegate.__init__(self)
 
     try:
-        with r.lock('ble-scan-lock', blocking_timeout=timeout+5) as lock:
+        if set_lock(timeout+5):
             scanner = Scanner().withDelegate(ScanDelegate())
             print('got here')
             scan_result = scanner.scan(float(timeout))
             print('got here too')
+            delete_lock()
             return(scan_result)
-    except redis.exceptions.LockError:
-        print("Failed to acquire lock")
-        return
+        else:
+            print("Failed to acquire lock")
+            return
     except BTLEManagementError:
         print("Got BTLEManagementError. This is probably due to two parallel sessions.")
         return
