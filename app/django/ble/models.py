2d3d1485a1ac18e3439fed7a7b948557b5c65a7d
Viktor Petersson <self@vpetersson.com>
2020-06-09 17:09:46 +0100

    Fixes migration and adds geofence filter back in.

--- app/django/ble/models.py
+++ app/django/ble/models.py
@@ -1,21 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.utils import timezone
-from django.db import models
 from django.conf import settings
+from django.db import models
 
 
 class Device(models.Model):
-    class Meta:
-        managed = True
-
     device_address = models.CharField(max_length=60)
     device_type = models.CharField(max_length=200)
     seen_first = models.DateTimeField(auto_now_add=True)
     seen_last = models.DateTimeField(auto_now=True)
     seen_counter = models.PositiveIntegerField(default=1)
-    #seen_within_geofence = models.BooleanField(default=False)
     ignore = models.BooleanField(default=False)
+    seen_within_geofence = models.BooleanField(default=False)
 
     def __str__(self):
         return self.device_address
@@ -29,14 +26,8 @@ class Device(models.Model):
     def seen_this_week(self):
         return self.seen_last.strftime('%W') == timezone.now().strftime('%W')
 
-    #def seen_within_geofence(self):
-    #    return self.seen_within_fence < settings.SENSITIVITY
-
 
 class ScanRecord(models.Model):
-    class Meta:
-        managed = True
-
     device = models.ForeignKey(
         Device,
         on_delete=models.CASCADE,
